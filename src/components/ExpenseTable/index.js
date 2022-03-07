fde0d0c8d54f7b8a07dae4a31be770f4a74156a3
Léia Ribeiro <leiaribeirot@gmail.com>
2022-03-07 15:04:31 -0400

    dev. Tabela de Despesa - req. 6

---
+++ src/components/ExpenseTable/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+class ExpenseTable extends Component {
+  render() {
+    return (
+      <table className="expenses-table">
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+      </table>
+    );
+  }
+}
+
+export default ExpenseTable;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from '../components/Header';
 import CurrencyForm from '../components/ExpenseForm';
+import ExpenseTable from '../components/ExpenseTable';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
-        <CurrencyForm />
-      </div>
+      <>
+        <div>
+          <Header />
+          <CurrencyForm />
+        </div>
+        <ExpenseTable />
+      </>
     );
   }
 }
